9b7eba439013c852c6863ce01f3990d5595eee81
Ramdcet <ramlakhanrajput@gmail.com>
2017-03-07 12:16:38 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function ($) {
+  $('body').instastream({
+		instaToken: '4526026830.1677ed0.02e1ac7cc0c24bc6ad2962b5b8192d43',
+		instaUser: '4526026830',
+		instaResults: 18,
+		instaMenu: 'yes'
+	});	
+});
